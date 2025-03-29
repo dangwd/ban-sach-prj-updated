95c29d8706441797c30ac6e069dece46b1d23dc9
Cheems <nhokcodon282002@gmail.com>
2025-03-29 19:09:33 +0700

    updated

---
+++ src/views/client/router/clientRouter.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        path: '/client',
+        component: () => import('@/views/client/layouts/AppLayout.vue'),
+        children: [
+            {
+                path: '/client',
+                name: 'home',
+                component: () => import('@/views/client/pages/HomePage.vue')
+            },
+            {
+                path: '/client/new-products',
+                name: 'new-products',
+                component: () => import('@/views/client/pages/NewProducts.vue')
+            },
+            {
+                path: '/client/products-list',
+                name: 'products-list',
+                component: () => import('@/views/client/pages/ProductsList.vue')
+            },
+            {
+                path: '/client/detail/:id',
+                name: 'item-detail',
+                component: () => import('@/views/client/pages/ProductDetail.vue')
+            },
+            {
+                path: '/client/payment',
+                name: 'payment',
+                component: () => import('@/views/client/pages/Payment.vue')
+            },
+            {
+                path: '/client/order-list',
+                name: 'orderlist',
+                component: () => import('@/views/client/pages/OrderList.vue')
+            },
+            {
+                path: '/client/payment-ing',
+                name: 'payment-ing',
+                component: () => import('@/views/client/pages/PaymentPage.vue')
+            },
+
+        ]
+    }
+];
